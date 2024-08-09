f9b7c154dcc4866a1c05d65fd31fb1a0ce244a2b
himanshu <himanshusinghofficial379@gmail.com>
2024-08-09 19:41:43 +0530

    save

---
+++ jwt/godmode/src/test/java/com/example/godmode/GodmodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.godmode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodmodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
